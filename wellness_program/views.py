5dd631346db8d86182019f9ab7190c4ce02b19f1
jashan <jashandhiman.hulk@gmail.com>
2021-12-01 14:45:37 +0530

    first page css

--- wellness_program/views.py
+++ wellness_program/views.py
@@ -1,5 +1,40 @@
 from django.shortcuts import render
 
-# Create your views here.
 def firstpage(request):
     return render(request, "firstpage.html")
+# def terms(request):
+#     return render(request, "terms.html")
+# def price(request):
+#     return render(request, "price.html")
+# def registration(request):
+#     return render(request, "registration.html")
+# def main_input(request):
+#     return render(request, "main_input.html")
+# def home(request):
+#     return render(request, "home.html")
+# def body_output(request):
+#     return render(request, "body_output.html")
+# def exercise(request):
+#     return render(request, "exercise.html")
+# def burn_output(request):
+#     return render(request, "burn_output.html")
+# def food(request):
+#     return render(request, "food.html")
+# def food_output(request):
+#     return render(request, "food_output.html")
+# def disease(request):
+#     return render(request, "disease.html")
+# def yoga(request):
+#     return render(request, "yoga.html")
+# def kriya(request):
+#     return render(request, "asans.html")
+# def yog_nidra(request):
+#     return render(request, "yognidra.html")
+# def system_wise(request):
+#     return render(request, "system-wise.html")
+# def upnishdic(request):
+#     return render(request, "upnishdik.html")
+# def natal(request):
+#     return render(request, "natal.html")
+# def more(request):
+#     return render(request, "more.html")
